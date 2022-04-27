c631ee5cb8f75814819415a8cf4ce9c388724ae7
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-04-26 23:19:35 -0400

    Worked more on the landing

--- components/Main.js
+++ components/Main.js
@@ -1,31 +1,20 @@
 // import Image from 'next/image'
+// import Exp1 from '../public/images/header-expertise-icon-1.svg'
+// import Exp2 from '../public/images/header-expertise-icon-2.svg'
+// import Exp3 from '../public/images/header-expertise-icon-3.svg'
 // import HeaderBG from '../public/images/header-background.jpg'
 
 import Link from 'next/link'
+import Landing from './mainComponents/Landing'
 
 export default function Main() {
   return (
-    <main id="home">
-      <header className="relative h-screen pt-12">
-        <div className="h-full flex flex-col justify-center items-center">
-          <h1 className="text-6xl text-center font-bold">
-            Award Winning <br /> Tech Support
-          </h1>
-          <p className="max-w-3xl mx-auto text-center text-lg my-5">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
-            facere consectetur consequuntur mollitia, dolores iure eaque nemo
-            assumenda reiciendis harum blanditiis, neque necessitatibus.
-          </p>
-          <div className="flex space-x-12 text-3xl">
-            <Link href="/">
-              <a className="px-6 py-1 bg-brandBlue">Services</a>
-            </Link>
-            <Link href="/" className="px-6 bg-brandBlue">
-              <a className="px-6 py-1 bg-brandBlue">Contact</a>
-            </Link>
-          </div>
-        </div>
-      </header>
+    // Home Container
+    <main>
+      <Landing />
+
+      {/* About Section */}
+      <section id="about"></section>
     </main>
   )
 }
---
+++ components/mainComponents/Landing.js
@@ -0,0 +1,74 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Exp1 from '../../public/images/header-expertise-icon-1.svg'
+import Exp2 from '../../public/images/header-expertise-icon-2.svg'
+import Exp3 from '../../public/images/header-expertise-icon-3.svg'
+
+export default function Landing() {
+  return (
+    <section id="home" className="relative h-screen pt-12">
+      {/* Flex Container */}
+      <div className="h-full flex flex-col justify-center items-center">
+        <h1 className="text-6xl text-center font-bold mb-6">
+          Award Winning <br /> Tech Support
+        </h1>
+        <div className="flex space-x-12 text-3xl">
+          <Link href="/">
+            <a className="px-6 py-1 rounded-xl bg-brandBlue">Services</a>
+          </Link>
+          <Link href="/" className="px-6 bg-brandBlue">
+            <a className="px-6 py-1 rounded-xl bg-brandBlue">Contact</a>
+          </Link>
+        </div>
+        {/* Expertise Features */}
+        <div className="w-full mt-6 flex flex-col space-y-2">
+          {/* Expertise 1 */}
+          <div className="px-3 py-3 flex bg-neutral-50 bg-opacity-50 shadow-md">
+            <div className="relative h-14 w-14 mr-3 float-left">
+              <Image
+                src={Exp1}
+                layout="fill"
+                objectFit="cover"
+                alt="Computer Icon"
+              />
+            </div>
+            <div>
+              <h4 className="text-2xl">24/7 Assistance</h4>
+              <p>Available 24/7, 365 for anything you may need</p>
+            </div>
+          </div>
+          {/* Expertise 2 */}
+          <div className="px-3 py-3 flex bg-neutral-50 bg-opacity-50 shadow-md">
+            <div className="relative h-14 w-14 mr-3 float-left">
+              <Image
+                src={Exp2}
+                layout="fill"
+                objectFit="cover"
+                alt="Folder Icon"
+              />
+            </div>
+            <div>
+              <h4 className="text-2xl">Private & Secure</h4>
+              <p>The most secure support on the market</p>
+            </div>
+          </div>
+          {/* Expertise 3 */}
+          <div className="px-3 py-3 flex bg-neutral-50 bg-opacity-50 shadow-md">
+            <div className="relative h-14 w-14 mr-3 float-left">
+              <Image
+                src={Exp3}
+                layout="fill"
+                objectFit="cover"
+                alt="File Icon"
+              />
+            </div>
+            <div>
+              <h4 className="text-2xl">Reliable Service</h4>
+              <p>Friendly assistance from professionals</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
