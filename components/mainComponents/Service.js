38f182c02d471c9fd2a6a1a470278cc9ef8dd64e
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-04-28 22:52:34 -0400

    About to start moving services into their own component

---
+++ components/mainComponents/Service.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+function Service({ services }) {}
+
+{
+  /* <div className="flex">
+            <div className="relative h-12 w-40 mr-2 mt-1.5">
+              <Image
+                src={Serv1}
+                layout="fill"
+                objectFit="cover"
+                alt="List Icon"
+              />
+            </div>
+            <div className="text-white">
+              <h4 className="font-bold text-xl">Easy Setup</h4>
+              <p className="text-sm leading-tight">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
+                odit repellat. Lorem ipsum dolor sit.
+              </p>
+            </div>
+          </div> */
+}
+
+export default Service
